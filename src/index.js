fad041867befa739920d9b7c5abc77a3863547fa
Wongelemengist Yilma <wongel.yilma2016@gmail.com>
2022-08-26 17:25:15 +0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
+import App from ".components.App";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
+  <Provider>
+    <App />
+  </Provider>
+);
