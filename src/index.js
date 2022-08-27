3baead4fa6daeb209c2f44e2079f07c52c63bc54
Wongelemengist Yilma <wongel.yilma2016@gmail.com>
2022-08-27 12:17:07 +0300

    Wired up the routes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
-import App from ".components.App";
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(
-  <Provider>
-    <App />
-  </Provider>
-);
+root.render(<App />);
