ad41132f4c386a7d60768f0ed6c2520d7c9bbf1d
Wongelemengist Yilma <wongel.yilma2016@gmail.com>
2022-08-31 15:07:15 +0300

    Creating a refactored form component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,17 +1,28 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../actions";
+import { fetchStream, updateRecord } from "../actions";
+import StreamForm from "./StreamForm";
 
 const StreamEdit = (props) => {
   useEffect(() => {
     props.fetchStream(props.match.params.id);
   }, []);
+  const onSubmit = (formValues) => {};
 
   console.log(props);
-  return props.stream ? <div>{props.stream.title}</div> : <div>Loading...</div>;
+  return props.stream ? (
+    <div>
+      <h3>Edit A Stream</h3>
+      <StreamForm initialValues={props.stream} onSubmit={onSubmit} />
+    </div>
+  ) : (
+    <div>Loading...</div>
+  );
 };
 
 const mapStateToProps = (state, { match }) => {
   return { stream: state.streams[match.params.id] };
 };
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, updateRecord })(
+  StreamEdit
+);
