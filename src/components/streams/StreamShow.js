60e7a68db319a26672d61d9875262c3370f4e223
Wongelemengist Yilma <wongel.yilma2016@gmail.com>
2022-09-01 11:57:37 +0300

    Setting up the stream showing component

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../actions";
+import streamReducer from "../reducers/streamReducer";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+const StreamShow = (props) => {
+  const id = props.match.params.id;
+  console.log(props);
+  useEffect(() => {
+    props.fetchStream(id);
+  }, []);
+  if (!props.stream) {
+    return <div>Loading...</div>;
+  } else {
+    const { title, description } = props.stream;
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
